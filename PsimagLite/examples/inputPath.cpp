38417d5e3c327116979554c2a42593cf031ca7ea
Gonzalo Alvarez <gz1@ornl.gov>
2026-04-10 17:20:34 -0400

    input path class added

---
+++ PsimagLite/examples/inputPath.cpp
@@ -0,0 +1,28 @@
+#include "InputPath.hpp"
+#include <iostream>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	PsimagLite::InputPath input_path;
+	std::string           usage
+	    = std::string("USAGE: ") + argv[0] + " -I path1 -I path2 ... -f filename\n";
+
+	std::string filename;
+	int         opt = 0;
+	while ((opt = getopt(argc, argv, "f:I:")) != -1) {
+		switch (opt) {
+		case 'f':
+			filename = optarg;
+			break;
+		case 'I':
+			input_path.push(optarg);
+			break;
+		default:
+			throw std::runtime_error(usage);
+		}
+	}
+
+	std::string result = input_path.findFirst(filename);
+	std::cout << result << "\n";
+}
---
+++ PsimagLite/src/InputPath.hpp
@@ -0,0 +1,46 @@
+#ifndef INPUTPATH_HPP
+#define INPUTPATH_HPP
+#include <algorithm>
+#include <filesystem>
+#include <string>
+#include <vector>
+
+namespace PsimagLite {
+
+class InputPath {
+public:
+
+	InputPath()
+	    : paths_(1, "")
+	{ }
+
+	void push(const std::string& path)
+	{
+		// path already in paths
+		if (std::find(paths_.begin(), paths_.end(), path) != paths_.end()) {
+			return;
+		}
+
+		paths_.push_back(path);
+	}
+
+	std::string findFirst(const std::string& file) const
+	{
+		for (const auto& dir : paths_) {
+			std::string           test_file = (dir.empty()) ? file : dir + "/" + file;
+			std::filesystem::path path      = test_file;
+			if (std::filesystem::exists(path)) {
+				return test_file;
+			}
+		}
+
+		throw std::runtime_error("File " + file
+		                         + " could not be found within available paths\n");
+	}
+
+private:
+
+	std::vector<std::string> paths_;
+};
+}
+#endif // INPUTPATH_HPP
